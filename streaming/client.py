91a3f619a70ae4d2e44026fa83bfe3eddef7ce64
choujo <jchouriopirela@gmail.com>
2019-11-30 15:24:08 -0400

    Cliente de prueba para conexion hacia el servidor de streaming

--- streaming/client.py
+++ streaming/client.py
@@ -1,12 +1,13 @@
+import sys
 import zmq
 import time
 
 url = 'tcp://0.0.0.0:5553'
 context = zmq.Context(io_threads=1)
-topic_filter = "video.mp4"
+topic_filter = sys.argv[1] if len(sys.argv) > 1 else "Client-video.mp4"
 streambe = context.socket(zmq.SUB)
 streambe.connect(url)
-streambe.setsockopt_string(zmq.SUBSCRIBE, "")
+streambe.setsockopt_string(zmq.SUBSCRIBE, topic_filter)
 
 frames = 0
 startTime = time.time()

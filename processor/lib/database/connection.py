73b322211f05df871d49a2c304f22d50ef155b45
Adrian Troconis <troconisbaltar@gmail.com>
2019-12-01 14:22:50 -0400

    Databases were changed

--- processor/lib/database/connection.py
+++ processor/lib/database/connection.py
@@ -1,10 +1,10 @@
 import mongoengine as mge
 
 def custom_connect(host='127.0.0.1', port=27017):
-    mge.connect('zeye-cloud-test', host=host, port=port)
+    mge.connect('uno', host=host, port=port)
     #mge.connect('_zeye-cloud', host=host, port=port)
 
 def test_default_connect(host='127.0.0.1', port=27017):
-	database = 'chou_test_database_2'
+	database = 'dos'
 	mge.connect(database, host=host, port=port)
 

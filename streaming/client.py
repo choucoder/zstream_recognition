77eadf8db64e37eea2686e64dd674e05b2b20be7
choujo <jchouriopirela@gmail.com>
2019-12-02 21:33:08 -0400

    Cliente para el servidor de streaming. uso: python3 client.py Client-camera_url

--- streaming/client.py
+++ streaming/client.py
@@ -1,6 +1,10 @@
+import cv2
 import sys
 import zmq
+import json
 import time
+from pickle import loads
+from utils.draw import drawFaces
 
 url = 'tcp://0.0.0.0:5553'
 context = zmq.Context(io_threads=1)
@@ -15,11 +19,21 @@ startTime = time.time()
 while True:
     msg = streambe.recv_multipart()
     client_address, frame, results = msg
-    
+    frame = loads(frame)
+    results = json.loads(results)
+
     elapsedTime = time.time() - startTime
     frames += 1
     fps = round(frames / elapsedTime, 2)
-    print("fps {}: {}".format(client_address, fps))
+
+    frame = drawFaces(frame, results['boxes'], results['ids'], results['names'])
+    cv2.putText(frame, 'fps: ' + str(fps), (int(10), int(20)),
+                cv2.FONT_HERSHEY_DUPLEX, 0.50, (190, 75, 23), 1)
+    
+    cv2.imshow(topic_filter, frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 streambe.close()
 context.term()
+cv2.destroyAllWindows()
